e1d3f24b1c76c0569ce599a620c0441c21edff4b
Cedric van Putten <me@bycedric.com>
2020-08-21 16:52:47 +0200

    feat: render schema description as markdown

--- src/manifest/schema.ts
+++ src/manifest/schema.ts
@@ -1,7 +1,12 @@
 import * as xdl from '@expo/xdl/build/project/ExpSchema';
+import jsonSchemaTraverse from 'json-schema-traverse';
 
-export interface ManifestSchema {
-  $schema: string,
+type JsonSchema = {
+	[key: string]: any,
+};
+
+export type ManifestSchema = {
+	$schema: string,
   type: 'object',
   version: string,
   additionalProperties: true,
@@ -20,12 +25,32 @@ export async function getSchema(sdkVersion: string) {
 }
 
 /**
- * Create a JSON schema object from XDL schema.
- * This will wrap the properties into `expo` validation.
+ * Create a vscode compatible JSON schema object from XDL schema.
+ * It also adds a `meta` property to know when this schema was generated.
+ * This will try to make some adjustments to the downloaded schema:
+ *   1. wrap the properties into an `expo` property for managed validation
+ *   2. copy `description` properties into `markdownDescription` to render content as markdown
+ *
+ * @remark When something fails in #2, it falls back to the original schema and only apply #1.
+ * @see https://github.com/microsoft/vscode/blob/1dff50d211472de4667db626ef2d6d32265eb0e6/src/vs/workbench/services/configuration/common/configurationExtensionPoint.ts#L64
  */
-export function createFromXdl(sdkVersion: string, xdlSchema: object): ManifestSchema {
+export function createFromXdl(sdkVersion: string, xdlSchema: JsonSchema): ManifestSchema {
+	// note: we need to create a copy, if something fails we need the original object
+	let enhancedSchema = JSON.parse(JSON.stringify(xdlSchema));
+
+	try {
+		jsonSchemaTraverse(enhancedSchema, (nestedSchema: JsonSchema) => {
+			if (nestedSchema.description && !nestedSchema.markdownDescription) {
+				nestedSchema.markdownDescription = nestedSchema.description;
+			}
+		});
+	} catch (error) {
+		// todo: add telemetry, fallback to original schema
+		enhancedSchema = xdlSchema;
+	}
+
   return {
-    $schema: 'http://json-schema.org/draft-07/schema#',
+		$schema: 'http://json-schema.org/draft-07/schema#',
     type: 'object',
     version: sdkVersion,
     additionalProperties: true,
@@ -33,7 +58,7 @@ export function createFromXdl(sdkVersion: string, xdlSchema: object): ManifestSc
       'expo',
     ],
     properties: {
-      expo: xdlSchema,
+      expo: enhancedSchema,
     },
   };
 }
