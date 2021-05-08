853465b17cdecc2198e5d4ed06755f98ea01c0b7
Cedric van Putten <me@bycedric.com>
2021-05-08 17:35:17 +0200

    chore: clean up testing logs

--- test/jest/runner.ts
+++ test/jest/runner.ts
@@ -5,6 +5,12 @@ const jestConfig = require('../../../jest.config');
 
 export async function run() {
   const logger = (line: string) => {
+    // note: vscode has built-in electron logging, which clutters ours
+    // let's filter them by using their markers.
+    if (line.includes('START_NATIVE_LOG') && line.includes('END_NATIVE_LOG')) {
+      return false;
+    }
+
     // note: process.std*.write doesn't add a newline,
     // console.log does so lets remove it from the output.
     console.log(line.replace(/^[\r\n]+|[\r\n]+$/g, ''));
