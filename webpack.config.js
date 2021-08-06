1388715c0f423351879500bd234cb8ad9f99c993
Cedric van Putten <me@bycedric.com>
2021-08-06 19:23:45 +0200

    fix: missing required libraries and make webpack work with xdl (#59)

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const { ESBuildMinifyPlugin } = require('esbuild-loader');
 const path = require('path');
-const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
   target: 'node',
@@ -19,7 +18,20 @@ module.exports = {
   externalsPresets: {
     node: true,
   },
-  externals: [nodeExternals(), { vscode: 'commonjs vscode' }],
+  externals: [
+    { vscode: 'commonjs vscode' },
+    // Dirty workaround to prevent modules being bundled,
+    // run `yarn webpack --mode development --stats-error-details` to update.
+    '@babel/helper-regex',
+    'emitter',
+    'fsevents',
+    /^webpack-hot-middleware/i,
+    /^webpack-plugin-serve/i,
+    // This library causes invalid JS, caused by https://github.com/jantimon/html-webpack-plugin/blob/8f8f7c53c4e4f822020d6da9de0304f8c23de08f/index.js#L133
+    // Webpack both shortens the `require: require` -> `require` and replaces require with an internal `__webpack_require(xxx)`.
+    // It's kinda stupid, `{ __webpack_require(xxx) }` is invalid, but we don't need this library within the bundle.
+    'html-webpack-plugin',
+  ],
   resolve: {
     extensions: ['.ts', '.js', '.json'],
   },
