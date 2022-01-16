95e13074489680737a664e83b872dab91e59e0e4
Cedric van Putten <me@bycedric.com>
2022-01-17 00:29:32 +0100

    chore: remove insiders and consolidate workflow

--- test/jest/runner.ts
+++ test/jest/runner.ts
@@ -4,6 +4,13 @@ import { runCLI } from 'jest';
 const jestConfig = require('../../../jest.config');
 
 export async function run() {
+  process.stderr.write = (line: string) => {
+    // note: process.std*.write doesn't add a newline,
+    // console.log does, so lets remove it from the output.
+    console.log(line.replace(/^[\r\n]+|[\r\n]+$/g, ''));
+    return true;
+  };
+
   const { results } = await runCLI({ ...jestConfig, runInBand: true }, [jestConfig.rootDir]);
   const failures = results.testResults.map((test) => test.failureMessage).filter(Boolean);
 
