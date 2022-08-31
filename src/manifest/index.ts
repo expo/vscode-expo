e31dc08d2eeadeb6898f32a11c5a052da51135c8
Cedric van Putten <me@bycedric.com>
2022-08-31 18:06:22 +0200

    refactor: update components and minor refactors (#129)
    * chore: update existing schema metadata to use same logic
    
    * chore: fix typo in resovled schema version
    
    * chore: update extension description from repository
    
    * chore: upgrade typescript and node
    
    * chore: align json traverse types
    
    * chore: update other dependencies
    
    * chore: drop json schema traverse types
    
    * chore: update description of manifest setup
    
    * refactor: setup components in parallel
    
    * chore: update lowest supported vscode to 1 year old
    
    * chore: update docs files
    
    * chore: swap to npm registry because of bad service

--- src/manifest/index.ts
+++ src/manifest/index.ts
@@ -3,8 +3,8 @@ import vscode from 'vscode';
 import { setupDefinition, setupPluginsValidation } from './configPlugins';
 
 /**
- * Download, store and activate the global Expo manifest JSON schema.
- * This will not check if the cache has an existing one, because we always want to fetch the latest.
+ * Set up enhancements for the App manifest file.
+ * This includes plugin validation, file path validation and other enhancements.
  */
 export async function setupXdlManifest(context: vscode.ExtensionContext) {
   setupPluginsValidation(context);
