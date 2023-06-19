cdcc117848d59c68d39b5504db607afe132faaef
Cedric van Putten <github@cedric.dev>
2023-06-19 20:43:34 +0200

    fix: avoid webpack interfering with any of the `@expo/*` packages

--- src/expo/packages.ts
+++ src/expo/packages.ts
@@ -1,32 +1,44 @@
-import { loadModuleFrom } from '../utils/module';
+// These packages are loaded from the user's project.
+// This means that these packages have to be marked as external in Webpack.
 
 /** Load the `@expo/config` package from the project directory */
-export function loadExpoConfig(projectRoot: string) {
-  return loadModuleFrom<typeof import('@expo/config')>(projectRoot, '@expo/config');
+export function loadExpoConfig(projectRoot: string): typeof import('@expo/config') {
+  return require(/* webpackIgnore: true */ require.resolve('@expo/config', {
+    paths: [projectRoot],
+  }));
 }
 
 /** Load the `@expo/config-plugins` package from the project directory */
-export function loadExpoConfigPlugins(projectRoot: string) {
-  return loadModuleFrom<typeof import('@expo/config-plugins')>(projectRoot, '@expo/config-plugins');
+export function loadExpoConfigPlugins(projectRoot: string): typeof import('@expo/config-plugins') {
+  return require(/* webpackIgnore: true */ require.resolve('@expo/config-plugins', {
+    paths: [projectRoot],
+  }));
 }
 
 /** Load the `@expo/config-plugins/build/utils/plugin-resolver` package from the project directory */
-export function loadExpoConfigPluginsResolver(projectRoot: string) {
-  return loadModuleFrom<typeof import('@expo/config-plugins/build/utils/plugin-resolver')>(
-    projectRoot,
-    '@expo/config-plugins/build/utils/plugin-resolver'
-  );
+export function loadExpoConfigPluginsResolver(
+  projectRoot: string
+): typeof import('@expo/config-plugins/build/utils/plugin-resolver') {
+  return require(/* webpackIgnore: true */ require.resolve(
+    '@expo/config-plugins/build/utils/plugin-resolver',
+    {
+      paths: [projectRoot],
+    }
+  ));
 }
 
 /** Load the `@expo/prebuild-config` package from the project directory */
-export function loadExpoPrebuildConfig(projectRoot: string) {
-  return loadModuleFrom<typeof import('@expo/prebuild-config')>(
-    projectRoot,
-    '@expo/prebuild-config'
-  );
+export function loadExpoPrebuildConfig(
+  projectRoot: string
+): typeof import('@expo/prebuild-config') {
+  return require(/* webpackIgnore: true */ require.resolve('@expo/prebuild-config', {
+    paths: [projectRoot],
+  }));
 }
 
 /** Load the `@expo/plist` package from the project directory */
-export function loadExpoPlist(projectRoot: string) {
-  return loadModuleFrom<typeof import('@expo/plist')>(projectRoot, '@expo/plist');
+export function loadExpoPlist(projectRoot: string): typeof import('@expo/plist') {
+  return require(/* webpackIgnore: true */ require.resolve('@expo/plist', {
+    paths: [projectRoot],
+  }));
 }
