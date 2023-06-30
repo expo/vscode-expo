4e47403d72a0d3c9908f1497e0a5a3a5c89833d9
Cedric van Putten <me@bycedric.com>
2023-06-30 17:36:04 +0200

    fix: filter duplicate devices on first occurence (#195)
    This is caused by Reanimated adding another inspector target, but Metro not being able to return that with a different name.

--- src/utils/array.ts
+++ src/utils/array.ts
@@ -2,3 +2,11 @@
 export function truthy<T>(value: T | null | undefined): value is T {
   return !!value;
 }
+
+/** Create a predicate to filter on first occurence of a generated value within an array. */
+export function uniqueBy<T>(key: (value: T) => string) {
+  return (value: T, index: number, list: T[]) => {
+    const identifier = key(value);
+    return list.findIndex((item) => identifier === key(item)) === index;
+  };
+}
