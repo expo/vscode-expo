fd89b6b16bf685e9f45da2e2945fb7ccb1abb37e
Cedric van Putten <github@cedric.dev>
2023-08-30 12:51:14 +0200

    chore: upgrade to `@vscode/test-electron@2.3.4`

--- test/jest/cli.ts
+++ test/jest/cli.ts
@@ -11,13 +11,7 @@ runTests({
     VSCODE_EXPO_DEBUG: 'vscode-expo*', // always enable the debugger
     VSCODE_EXPO_TELEMETRY_KEY: '', // always disable telemetry in tests
   },
-  launchArgs: [
-    '--disable-extensions',
-    '--disable-gpu',
-    '--disable-workspace-trust',
-    '--no-xshm',
-    path.resolve(rootDir, './test/fixture'),
-  ],
+  launchArgs: ['--disable-extensions', path.resolve(rootDir, './test/fixture')],
   version: process.env.VSCODE_VERSION,
   // Reporter is disabled in CI because it's super slow
   reporter: process.env.CI ? new SilentReporter() : undefined,
