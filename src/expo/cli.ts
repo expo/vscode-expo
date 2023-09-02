2179d90ee50072bb04d11a01a49c47d3acd3a8a8
Cedric van Putten <github@cedric.dev>
2023-09-02 15:43:10 +0200

    refactor: drop the unhelpful debug logging from spawn expo cli

--- src/expo/cli.ts
+++ src/expo/cli.ts
@@ -1,9 +1,5 @@
 import cp from 'child_process';
 
-import { debug } from '../utils/debug';
-
-const log = debug.extend('project');
-
 /**
  * Execute an Expo CLI command using the `npx expo <command> <...argsOrFlags>` syntax.
  * This is useful to ask the project for data, like introspection.
@@ -13,9 +9,7 @@ export function spawnExpoCli(
   argsOrFlags: string[],
   options: cp.ExecSyncOptions = {}
 ) {
-  log(`Executing 'npx expo ${command} ${argsOrFlags.join(' ')}'`);
-
-  const result = cp.spawnSync('npx', ['--no-install', 'expo', command, ...argsOrFlags], {
+  const result = cp.spawnSync('npx', ['expo', command, ...argsOrFlags], {
     ...options,
     env: {
       ...process.env,
@@ -25,8 +19,6 @@ export function spawnExpoCli(
       EXPO_NO_TELEMETRY: 'true',
     },
   });
-
-  log(`Received status ${result.status} from 'npx expo ${command} ${argsOrFlags.join(' ')}'`);
 
   if (result.error) {
     throw result.error;
