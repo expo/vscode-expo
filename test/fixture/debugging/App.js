c9961961d16192397b3479fd7c6284fc9b65d4b5
Cedric van Putten <github@cedric.dev>
2024-03-06 19:24:54 +0100

    refactor: update all fixtures to SDK 50

--- test/fixture/debugging/App.js
+++ test/fixture/debugging/App.js
@@ -1,10 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+function onButtonPress() {
+  const hello = 'world';
+  console.log({ hello });
+  return true;
+}
 
 export function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Button title="Press me" onPress={onButtonPress} />
       <StatusBar style="auto" />
     </View>
   );
