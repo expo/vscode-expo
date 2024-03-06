feceea42cc1309cfd0d498f4623ad632bed33a7e
Cedric van Putten <me@bycedric.com>
2024-03-06 21:21:12 +0100

    refactor(fixtures): upgrade all fixtures to SDK 50 (#246)
    * refactor: update all fixtures to SDK 50
    
    * chore: update all test snapshot for SDK 50
    
    * fix: use full typescript extension name to avoid confusing vscode
    
    * chore: add debugging fixture with logs and breakpoint test

--- test/fixture/debugging/App.js
+++ test/fixture/debugging/App.js
@@ -1,10 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+function onButtonPress() {
+  // Breakpoints should work here
+  const hello = 'world';
+  console.log('This should be visible in Vscodes debug console', { hello });
+  return true;
+}
 
 export function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Button title="Press me" onPress={onButtonPress} />
       <StatusBar style="auto" />
     </View>
   );
