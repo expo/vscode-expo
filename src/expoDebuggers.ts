486874a1bec2094b0e197f9d43f3eabf5114b9c1
Cedric van Putten <me@bycedric.com>
2024-12-19 14:56:27 +0100

    fix: update device filtering logic to select inspectable device for SDK 52 (#266)
    * fix: update device filtering logic to select inspectable device for SDK 52
    
    * chore: clean up code and resolve patch-package issue

--- src/expoDebuggers.ts
+++ src/expoDebuggers.ts
@@ -1,12 +1,7 @@
 import fs from 'fs';
 import vscode from 'vscode';
 
-import {
-  fetchDevicesToInspect,
-  askDeviceByName,
-  inferDevicePlatform,
-  fetchDevicesToInspectFromUnknownWorkflow,
-} from './expo/bundler';
+import { fetchDevicesToInspect, askDeviceByName, inferDevicePlatform } from './expo/bundler';
 import {
   ExpoProjectCache,
   ExpoProject,
@@ -249,15 +244,14 @@ async function waitForDevice(config: ExpoDebugConfig) {
 
 async function pickDevice(config: ExpoDebugConfig) {
   const bundlerHost = config.bundlerHost ?? '127.0.0.1';
+  const bundlerPort = config.bundlerPort ?? '8081';
 
-  // Either fetch from user-specified port, or try both `19000` and `8081`.
-  const devices = config.bundlerPort
-    ? await fetchDevicesToInspect({ host: bundlerHost, port: config.bundlerPort }).catch(() => {
-        throw new Error(`waiting for bundler on ${config.bundlerHost}:${config.bundlerPort}...`);
-      })
-    : await fetchDevicesToInspectFromUnknownWorkflow({ host: bundlerHost }).catch(() => {
-        throw new Error(`waiting for bundler on ${config.bundlerHost}...`);
-      });
+  const devices = await fetchDevicesToInspect({
+    host: bundlerHost,
+    port: bundlerPort,
+  }).catch(() => {
+    throw new Error(`waiting for bundler on ${bundlerHost}:${bundlerPort}...`);
+  });
 
   if (devices.length === 1) {
     log('Picking only device available:', devices[0].deviceName);
